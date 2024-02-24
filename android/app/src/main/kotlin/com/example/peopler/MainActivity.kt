bd85772bfa8bc71f08cac5f22e42fc72917ceb87
cencool <cencul@protonmail.com>
2024-02-24 11:21:10 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/peopler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.peopler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/person.dart
@@ -0,0 +1,25 @@
+class Person {
+  int? id;
+  String surname;
+  String? name;
+  String gender;
+  String? place;
+  String owner;
+
+  Person(
+      {this.id,
+      required this.surname,
+      this.name,
+      this.place,
+      required this.gender,
+      required this.owner});
+
+  factory Person.fromJson(Map<String, dynamic> json) => Person(
+        id: json["id"],
+        surname: json["surname"],
+        name: json["name"],
+        place: json["place"],
+        gender: json["gender"],
+        owner: json["owner"],
+      );
+}
