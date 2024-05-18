7fc53937ab09fe0c8ca7d18c3d6bc65fd221c912
cencool <cencul@protonmail.com>
2024-05-18 17:58:15 +0200

    attachment thumbnail list and auth in Appstate

--- lib/widgets/attachment_tab.dart
+++ lib/widgets/attachment_tab.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:peopler/globals/app_state.dart';
+import 'package:peopler/models/api.dart';
 import 'package:peopler/models/person.dart';
 import 'package:peopler/models/person_attachment.dart';
 import 'package:provider/provider.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class AttachmentTab extends StatefulWidget {
   const AttachmentTab({super.key});
@@ -23,10 +25,18 @@ class _AttachmentTabState extends State<AttachmentTab> {
         future: attachmentList,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<Text> attachmentFiles = [];
+            String authString = context.read<AppState>().authString;
+            List<Widget> attachmentFiles = [];
             for (final item in snapshot.data!) {
               attachmentFiles
                   .add(Text('${item.id},${item.personId},${item.fileCaption},${item.fileName}'));
+              attachmentFiles.add(FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(
+                  '${Api.attachmentUrl}/send-thumbnail?fileId=${item.id}&${DateTime.now().millisecondsSinceEpoch}',
+                  headers: {'Authorization': 'Basic $authString'},
+                ),
+              ));
             }
             return ListView(children: attachmentFiles);
           } else {
