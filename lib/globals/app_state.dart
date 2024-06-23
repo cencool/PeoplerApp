335cebccb515e78d90cbdbcf1616635bc8fe55f4
cencool <cencul@protonmail.com>
2024-06-23 19:17:26 +0200

    prep for change_notifier

--- lib/globals/app_state.dart
+++ lib/globals/app_state.dart
@@ -16,3 +16,65 @@ class AppState {
   PlutoGridStateManager? personSearchListStateManager;
   String authString = '';
 }
+
+/*
+class AppState extends ChangeNotifier {
+  GlobalKey<ScaffoldMessengerState> _messengerKey = GlobalKey<ScaffoldMessengerState>();
+  get messengerKey => _messengerKey;
+  set messengerKey(val) {
+    _messengerKey = val;
+    notifyListeners();
+  }
+
+  Person _activePerson = Person.dummy();
+  Person get activePerson => _activePerson;
+  set activePerson(Person person) {
+    _activePerson = person;
+    notifyListeners();
+  }
+
+  PersonDetail _activePersonDetail = PersonDetail.dummy(-1);
+  PersonDetail get activePersonDetail => _activePersonDetail;
+  set activePersonDetail(PersonDetail detail) {
+    _activePersonDetail = detail;
+    notifyListeners();
+  }
+
+  RelationRecord _activeRelationRecord = RelationRecord.dummy();
+  RelationRecord get activeRelationRecord => _activeRelationRecord;
+  set activeRelationRecord(RelationRecord record) {
+    _activeRelationRecord = record;
+    notifyListeners();
+  }
+
+  PersonItem _activePersonItem = PersonItem.dummy();
+  PersonItem get activePersonItem => _activePersonItem;
+  set activePersonItem(PersonItem item) {
+    _activePersonItem = item;
+    notifyListeners();
+  }
+
+  PlutoGridStateManager? _relationTableStateManager;
+  PlutoGridStateManager? get relationTableStateManager => _relationTableStateManager;
+  set relationTableStateManager(PlutoGridStateManager? manager) {
+    _relationTableStateManager = manager;
+    notifyListeners();
+  }
+
+  PlutoGridStateManager? _personListStateManager;
+  PlutoGridStateManager? get personListStateManager => _personListStateManager;
+  set personListStateManager(PlutoGridStateManager? manager) {
+    _personListStateManager = manager;
+    notifyListeners();
+  }
+
+  PlutoGridStateManager? _personSearchListStateManager;
+  PlutoGridStateManager? get personSearchListStateManager => _personSearchListStateManager;
+  set personSearchListStateManager(PlutoGridStateManager? manager) {
+    _personSearchListStateManager = manager;
+    notifyListeners();
+  }
+
+  String authString = '';
+}
+*/
